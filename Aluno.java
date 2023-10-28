5a1692aced5f486c57636cf335389c794b5d7cbc
Rian Gomes <80797632+riangomesz@users.noreply.github.com>
2023-10-27 21:20:36 -0300

    Create Aluno.java

---
+++ Aluno.java
@@ -0,0 +1,15 @@
+
+public class Aluno {
+int RA;
+String nome;
+
+public Aluno(int RA, String nome) {
+	this.RA = RA;
+	this.nome = nome;
+}
+public String toString() {
+	return RA + " - " + nome;
+	
+}
+}
+
